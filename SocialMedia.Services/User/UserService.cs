62d404e6e51ab8f687b6895bb5de10452e84b1c4
Sy Howard <symanthacrh@gmail.com>
2023-11-01 12:33:17 -0400

    Register

--- SocialMedia.Services/User/UserService.cs
+++ SocialMedia.Services/User/UserService.cs
@@ -1,8 +1,35 @@
-using SocialMedia.Services.User;
+using Microsoft.AspNetCore.Identity;
+using SocialMedia.Data;
+using SocialMedia.Data.Entities;
+using SocialMedia.Models.User;
 
 namespace SocialMedia.Services.User;
 
 public class UserService : IUserService
 {
-    
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<UserEntity> _userManager;
+    private readonly SignInManager<UserEntity> _signInManager;
+
+    public UserService(ApplicationDbContext context,
+                        UserManager<UserEntity> userManager,
+                        SignInManager<UserEntity> signInManager)
+    {
+        _context = context;
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    public async Task<bool> RegisterUserAsync(UserRegister model)
+    {
+        UserEntity entity = new()
+        {
+            Email = model.Email
+
+        };
+        
+        IdentityResult registerResult = await _userManager.CreateAsync(entity, model.Password);
+
+        return registerResult.Succeeded;
+    }
 }
