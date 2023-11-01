b0f959ecfc7b20d56d2df8a303a849e8310bd849
Sy Howard <symanthacrh@gmail.com>
2023-11-01 10:41:58 -0400

    initial commit

---
+++ SocialMedia.Data/Entities/UserEntity.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace SocialMedia.Data.Entities;
+
+public class UserEntity : IdentityUser<int>
+{
+    [MaxLength(100)]
+    public string? FristName {get; set;}
+    [MaxLength(100)]
+    public string? LastName {get; set;}
+}
