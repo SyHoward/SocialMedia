b6e09133123457b075e2889fecfed55dc07b3098
Austin Carie <austincarie0209@gmail.com>
2023-11-01 22:17:50 -0400

    Get All Posts functionality

--- SocialMedia.WebAPI/Controllers/PostController.cs
+++ SocialMedia.WebAPI/Controllers/PostController.cs
@@ -2,18 +2,23 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SocialMedia.Services.Post;
 
-namespace SocialMedia.WebAPI.Controllers
+namespace SocialMedia.WebAPI.Controllers;
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class PostController : ControllerBase
 {
-    [Authorize]
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PostController : ControllerBase
-    {
-        private readonly IPostService _postService;
+    private readonly IPostService _postService;
 
-        public PostController(IPostService postService)
-        {
-            _postService = postService;
-        }
+    public PostController(IPostService postService)
+    {
+        _postService = postService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllPosts()
+    {
+        var posts = await _postService.GetAllPostsAsync();
+        return Ok(posts);
     }
 }
