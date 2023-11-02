14e3ec42c883a015aa5ce0033896ec46fa5fdbd4
Scott Allen <sallen1851@gmail.com>
2023-11-02 17:05:14 -0400

    Using directives added

--- SocialMedia.Models/Like/Like.cs
+++ SocialMedia.Models/Like/Like.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace SocialMedia.Models
 {
-    public class Like
+  public class Like
     {
+        [Key, Column(Order = 0)]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
         
+        [Key, Column(Order = 1)]
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
     }
 }
